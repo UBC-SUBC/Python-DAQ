559882434860626a86a4929c05e51710ec92aed5
tu1026 <s31302@gmail.com>
2023-05-26 15:10:47 -0700

    no unit circle

--- virtual_test_bench.py
+++ virtual_test_bench.py
@@ -19,13 +19,13 @@ def plot_yaw_ptich_row(euler, ax):
     pitch = euler[1]
     roll = euler[2]
     
-    # Plot unit circle
-    u = np.linspace(0, 2 * np.pi, 100)
-    v = np.linspace(0, np.pi, 100)
-    x = 5 * np.outer(np.cos(u), np.sin(v))
-    y = 5 * np.outer(np.sin(u), np.sin(v))
-    z = 5 * np.outer(np.ones(np.size(u)), np.cos(v))
-    ax.plot_surface(x, y, z, color='lightgray', alpha=0.5)
+    # # Plot unit circle
+    # u = np.linspace(0, 2 * np.pi, 100)
+    # v = np.linspace(0, np.pi, 100)
+    # x = 5 * np.outer(np.cos(u), np.sin(v))
+    # y = 5 * np.outer(np.sin(u), np.sin(v))
+    # z = 5 * np.outer(np.ones(np.size(u)), np.cos(v))
+    # ax.plot_surface(x, y, z, color='lightgray', alpha=0.5)
     
     x = np.sin(yaw) * np.sin(pitch) * np.sin(roll) + np.cos(yaw) * np.cos(roll)
     y = np.sin(yaw) * np.sin(pitch) * np.cos(roll) - np.cos(yaw) * np.sin(roll)
