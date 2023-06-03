8bc10ac82d81e61385c2ff48c296557060f67f23
tu1026 <s31302@gmail.com>
2023-06-03 12:51:04 -0700

    updated depth sensor module

--- depth_sensor.py
+++ depth_sensor.py
@@ -3,9 +3,28 @@ from importlib import import_module
 class Depth_Sensor:
     
     def __init__(self) -> None:
-        self.SMbus = import_module("smbus2.SMBus")
+        self.ms5837 = import_module("ms5837")
+        self.sensor = self.ms5837.MS5837_30BA(1)
     
     
-
+    def outputDict(self):
+        freshwaterDepth = self.sensor.depth() # default is freshwater
+        self.sensor.setFluidDensity(self.ms5837.DENSITY_SALTWATER)
+        saltwaterDepth = self.sensor.depth() # No nead to read() again
+        self.sensor.setFluidDensity(1000) # kg/m^3
+        print(("Depth: %.3f m (freshwater)  %.3f m (saltwater)") % (freshwaterDepth, saltwaterDepth))
+        
+        return {
+            'fresh_depth': freshwaterDepth,
+            'salt_depth': saltwaterDepth
+        }
+        
+        
+        
+        
 class Dummy_Depth_Sensor:
-    
+    def outputDict(self):
+        return {
+            'fresh_depth': -9999,
+            'salt_depth': -9999
+        }
