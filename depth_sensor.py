3d1729161c2070977b521a8f219c0cf6979a8219
tu1026 <s31302@gmail.com>
2023-06-03 13:11:49 -0700

    initlize sensor

--- depth_sensor.py
+++ depth_sensor.py
@@ -5,6 +5,7 @@ class Depth_Sensor:
     def __init__(self) -> None:
         self.ms5837 = import_module("ms5837")
         self.sensor = self.ms5837.MS5837_02BA(1)
+        self.sensor.init()
     
     
     def outputDict(self):
